6b8b78827aca881871a28eb616ed15bcb29bcdf4
chenjiandongx <chenjiandongx@qq.com>
2018-03-23 00:17:46 +0800

    集成为一个端口

--- server.js
+++ server.js
@@ -2,32 +2,30 @@ const http = require('http');
 const path = require('path');
 const request = require('got');
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 8010;
-const imgPort = process.env.PORT || 8011;
+const HOSTNAME = '0.0.0.0';
+const PORT = process.env.PORT || 8010;
 
 const apiServer = http.createServer((req, res) => {
-    const url = 'http://news-at.zhihu.com/api/4' + req.url;
-    request.get(url).then(response => {
-        res.setHeader('Content-Type', 'text/plain');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.end(response.body);
-    }).catch(error => {});
-}).listen(port, hostname, () => {
-    console.log(`接口代理运行在 http://${hostname}:${port}/`)
-});
+    const url = req.url.split('/img/')[1];
+    if (url === undefined) {
+        const apiUrl = 'http://news-at.zhihu.com/api/4' + req.url;
+        request.get(apiUrl).then(response => {
+            res.setHeader('Content-Type', 'text/plain;charset=utf8');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.end(response.body);
+        }).catch(error => {});
+    } else {
+        const imgUrl = req.url.split('/img/')[1];
+        request.get(imgUrl, { encoding: null }).then(response => {
+            res.setHeader('Content-Type', 'image/jpeg')
+            res.setHeader('Content-Type', 'image/gif');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.end(response.body);
+        }).catch(error => {});
+    }
 
-// const imgServer = http.createServer((req, res) => {
-//     const url = req.url.split('/img/')[1]
-//     request.get(url, { encoding: null }).then(response => {
-//         res.setHeader('Content-Type', 'image/jpeg')
-//         res.setHeader('Content-Type', 'image/gif');
-//         res.setHeader('Access-Control-Allow-Origin', '*');
-//         res.end(response.body);
-//     }).catch(error => {});
-//
-// }).listen(imgPort, hostname, () => {
-//     console.log(`图片代理运行在 http://${hostname}:${imgPort}/`)
-// });
+}).listen(PORT, HOSTNAME, () => {
+    console.log(`接口代理运行在 http://${HOSTNAME}:${PORT}/`)
+});
 
 console.log(process.env.PORT)
