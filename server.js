c281a57d8cc164f2525b69f83073d47170c1e38d
chenjiandongx <chenjiandongx@qq.com>
2018-03-22 23:59:51 +0800

    测试端口

--- server.js
+++ server.js
@@ -7,29 +7,27 @@ const port = process.env.PORT || 8010;
 const imgPort = process.env.PORT || 8011;
 
 const apiServer = http.createServer((req, res) => {
-        const url = 'http://news-at.zhihu.com/api/4' + req.url;
-        request.get(url).then(response => {
+    const url = 'http://news-at.zhihu.com/api/4' + req.url;
+    request.get(url).then(response => {
         res.setHeader('Content-Type', 'text/plain');
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.end(response.body);
-}).catch(error => {
-});
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.end(response.body);
+    }).catch(error => {});
 }).listen(port, hostname, () => {
     console.log(`接口代理运行在 http://${hostname}:${port}/`)
 });
 
-const imgServer = http.createServer((req, res) => {
-        const url = req.url.split('/img/')[1]
-        request.get(url, {encoding: null}).then(response => {
-        res.setHeader('Content-Type', 'image/jpeg')
-res.setHeader('Content-Type', 'image/gif');
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.end(response.body);
-}).catch(error => {
-});
-
-}).listen(imgPort, hostname, () => {
-    console.log(`图片代理运行在 http://${hostname}:${imgPort}/`)
-});
+// const imgServer = http.createServer((req, res) => {
+//     const url = req.url.split('/img/')[1]
+//     request.get(url, { encoding: null }).then(response => {
+//         res.setHeader('Content-Type', 'image/jpeg')
+//         res.setHeader('Content-Type', 'image/gif');
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         res.end(response.body);
+//     }).catch(error => {});
+//
+// }).listen(imgPort, hostname, () => {
+//     console.log(`图片代理运行在 http://${hostname}:${imgPort}/`)
+// });
 
 console.log(process.env.PORT)
